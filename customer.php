644b584d5f9f0643829e38f278c3e4a0ae34347b
ahmedpk <ahmed883@gmail.com>
2013-10-06 02:04:01 +0500

    Updated Code

--- customer.php
+++ customer.php
@@ -2,27 +2,27 @@
 class Customer
 {
 	Public $customer_data = array("username", "password");
-	
+
 	// constructing the class
 	Public function __construct($new_username) {
 		$this->customer_data[username] = $new_username;
 	}
-	
+
 	// to set customer password
 	Public function setPassword($new_password) {
 		$this->customer_data[password] = $new_password;
 	}
-	
+
 	// To verify customer password
 	Public function checkPassword($get_password) {
-		return ( $this->customer_data[password]==$get_password )
-		?true
-		:false;
+		return ($this->customer_data[password]==$get_password)
+		? true
+		: false;
 	}
-	
+
 	// Summary of customer
 	Public function getSummary() {
-		return "{$this->customer_data[username]} : {$this->customer_data[password]}";
+		return "{$this->customer_data[username]}:{$this->customer_data[password]}";
 	}
 }
 
