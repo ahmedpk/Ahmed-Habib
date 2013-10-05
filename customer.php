fdb87c7b9987a8185fc79f6de86d7063fed702fc
ahmedpk <ahmed883@gmail.com>
2013-10-06 00:48:38 +0500

    Changed Code to meet requirements

--- customer.php
+++ customer.php
@@ -1,26 +1,29 @@
 <?php
-class Customer{
-	Public $customer_name;
-	Public $customer_pass;
+class Customer
+{
+	Public $customer_data = array("username", "password");
 	
 	// constructing the class
-	Public function __construct($new_name){
-		$this->customer_name = $new_name;
+	Public function __construct($new_username) {
+		$this->customer_data[username] = $new_username;
 	}
 	
 	// to set customer password
-	Public function setPassword($new_pass){
-		$this->customer_pass = $new_pass;
+	Public function setPassword($new_password) {
+		$this->customer_data[password] = $new_password;
 	}
 	
 	// To verify customer password
-	Public function checkPassword($get_pass){
-		return ($this->customer_pass==$get_pass)?true:false;
+	Public function checkPassword($get_password) {
+		return ( $this->customer_data[password]==$get_password )
+		?true
+		:false;
 	}
 	
 	// Summary of customer
-	Public function getSummary(){
-		return $this->customer_name .":".$this->customer_pass;
+	Public function getSummary() {
+		return "{$this->customer_data[username]} : {$this->customer_data[password]}";
 	}
 }
+
 ?>
