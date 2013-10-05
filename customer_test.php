e623a631cf9f95ca212d02a892b742c49e4b89eb
ahmedpk <ahmed883@gmail.com>
2013-10-06 02:05:19 +0500

    Updated Code. New changes

--- customer_test.php
+++ customer_test.php
@@ -1,31 +1,23 @@
 <?php
-
 require_once(dirname(__FILE__).'/customer.php');
 
-$customer = new Customer('David');
+$customer = new Customer('David'); // Init new customer
+$customer->setPassword('123'); // Password set
 
-$customer->setPassword('123');
+	// Checking Password
+	echo "123 Password ";
+	echo ($customer->checkPassword('123')) ? "Correct" : "Incorrect";
+	echo "<br/>";
+	
+	echo "12 Password ";
+	echo ($customer->checkPassword('12')) ? "Correct" : "Incorrect";
+	echo "<br/>";
 
-// Checking password if valid
-echo "123 Password ";
-($customer->checkPassword('123'))
-?"Correct"
-:"Incorrect";
-echo "<br/>";
+	echo "122 Password ";
+	echo ($customer->checkPassword('122')) ? "Correct" : "Incorrect";
+	echo "<br/>";
 
-echo "12 Password ";
-($customer->checkPassword('12'))
-?"Correct"
-:"Incorrect";
-echo "<br/>";
-
-echo "122 Password ";
-($customer->checkPassword('122'))
-?"Correct"
-:"Incorrect";
-echo "<br/>";
-
-// Printing the summary
-echo $customer->getSummary();
+	// Printing the summary
+	echo $customer->getSummary();
 
 ?>
