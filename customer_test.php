622d84ced2ddb1390422c894f1eb5d991ec81961
ahmedpk <ahmed883@gmail.com>
2013-10-06 00:57:53 +0500

    Updates to meet requirements and standards.

--- customer_test.php
+++ customer_test.php
@@ -1,22 +1,31 @@
 <?php
+
 require_once(dirname(__FILE__).'/customer.php');
 
-// Creating new customer
 $customer = new Customer('David');
-// Setting up password
+
 $customer->setPassword('123');
 
 // Checking password if valid
-$result1 = ($customer->checkPassword('123'))?"Password True":" Password False"; //check 123
-echo "Password 123 -> $result1 <br/>";
+echo "123 Password ";
+($customer->checkPassword('123'))
+?"Correct"
+:"Incorrect";
+echo "<br/>";
 
-$result2 = ($customer->checkPassword('12'))?"Password True":"Password False"; // check 12
-echo "Password 12 -> $result2 <br/>";
+echo "12 Password ";
+($customer->checkPassword('12'))
+?"Correct"
+:"Incorrect";
+echo "<br/>";
 
-$result3 = ($customer->checkPassword('122'))?"Password True":"Password False"; // check 122
-echo "Password 122 -> $result3 <br/>";
+echo "122 Password ";
+($customer->checkPassword('122'))
+?"Correct"
+:"Incorrect";
+echo "<br/>";
 
 // Printing the summary
-echo "Summary -> ".$customer->getSummary();
+echo $customer->getSummary();
 
 ?>
