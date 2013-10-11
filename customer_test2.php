eacc84b01ce5446fc74189a8cfdc6b5d698428c2
ahmedpk <ahmed883@gmail.com>
2013-10-12 01:09:19 +0500

    Updated after code review.

--- customer_test2.php
+++ customer_test2.php
@@ -4,20 +4,23 @@ require_once(dirname(__FILE__).'/customer.php');
 
 $customer = new Customer;
 
-if ($customer->fetchData($_GET['username'])) {
-  echo 'Full Name: '.$customer->get('name').'<br/>';
-  echo 'Username: '.$customer->get('username').' <br/>';
-  echo 'Password: '.$customer->get('password').' <br/>';
-  echo 'Email: '.$customer->get('email').' <br/>';
-  echo 'Phone Number: '.$customer->get('phone').' <br/>';
+if (isset($_GET['username'])) {
+	if ($customer->fetchData($_GET['username'])) {
+		echo 'Full Name: '.$customer->get('name').'<br/>';
+		echo 'Username: '.$customer->get('username').' <br/>';
+		echo 'Password: '.$customer->get('password').' <br/>';
+		echo 'Email: '.$customer->get('email').' <br/>';
+		echo 'Phone Number: '.$customer->get('phone').' <br/>';
+	} else {
+		echo 'Customer not found, please input username!!<br/>';
+	}
+} else {
+	echo 'Please enter username below.';
 }
-else {
-  echo 'Customer not found, please input username!!<br/>';
-}
-
-echo '<form action="/ahmed/customer_test2.php" method="get">
-<input type="text" name="username"/><br/>
-<input type="submit" value="Get Summary"/>
-</form>';
 
 ?>
+
+<form action="/ahmed/customer_test2.php" method="get">
+<input type="text" name="username"/><br/>
+<input type="submit" value="Get Summary"/>
+</form>
