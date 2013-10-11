0d3c8ad39b5588600022338061780c7c37eb5a64
ahmedpk <ahmed883@gmail.com>
2013-10-12 01:03:00 +0500

    Update file after code review.

--- customer.php
+++ customer.php
@@ -2,45 +2,44 @@
 
 class Customer {
   public $customerData = array();
-  public $dataDir = 'data/';
-	public $allowedKeys = array('username', 'password', 'name', 'email', 'phone');
+  public static $DATA_DIR = 'data/';
+  public static $ALLOWED_KEYS = array('username', 'password', 'name', 'email', 'phone');
 
-	public function set($key, $value) {
-		if (!empty($value) && in_array($key, $this->allowedKeys)) {
-			$this->customerData["$key"] = $value;
-			return true;
-		}
-		else {
+  public function set($key, $value) {
+   if (!empty($value) && in_array($key, self::$ALLOWED_KEYS)) {
+      $this->customerData[$key] = $value;
+	return true;
+    } else {
 			return false;
 		}
 	}
 
 	public function get($key) {
-		if (in_array("$key", $this->allowedKeys)) {
-			return $this->customerData["$key"];
+		if (in_array($key, self::$ALLOWED_KEYS)) {
+			return $this->customerData[$key];
+		} else {
+			return false;
 		}
-		return false;
 	}	
 	
   public function saveData() {
-		$getUsername = $this->get('username');
-    $fileName = $this->dataDir ."$getUsername.txt";
+		$username = $this->get('username');
+    $fileName = self::$DATA_DIR . $username .'txt';
     $fileHandler = fopen($fileName, 'w') or die('Cannot open file');
     $saveContent = serialize($this->customerData);
     return fwrite($fileHandler, $saveContent);
 		fclose($fileHandler);
   }
   
-  public function fetchData($getUsername) {
-    $customerFile = $this->dataDir ."$getUsername.txt"; 
+  public function fetchData($username) {
+    $customerFile = self::$DATA_DIR . $username .'txt';
 		$fileHandler = fopen($customerFile, 'r'); 
     $readData = fread($fileHandler, 8192); 
     fclose($fileHandler);
 		if ($fileHandler) {
 			$this->customerData = unserialize($readData);
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
   }
