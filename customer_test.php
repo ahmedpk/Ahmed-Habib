50cd951133acfe82aa75a25eb9f6823725c4ba93
ahmedpk <ahmed883@gmail.com>
2013-10-12 01:06:40 +0500

    Updated file after code review!

--- customer_test.php
+++ customer_test.php
@@ -8,22 +8,21 @@ if (isset($_POST['f_submit'])) {
   if ($customer->set('username', $_POST['c_username']) && $customer->set('password', $_POST['c_password']) && $customer->set('name', $_POST['c_name']) && $customer->set('email', $_POST['c_email']) && $customer->set('phone', $_POST['c_phone'])) {
 		if ($customer->saveData()) {
 			echo 'Your data is saved successfully, you may find your data <a href="/ahmed/customer_test2.php?username='.$customer->get('username').'">here</a>';
-		}
-		else {
+		} else {
 			echo 'Data was not saved for odd reasons. Try again!';
 		}
-  }
-  else {
+  } else {
     echo 'Fields Missing!!<br/>';
   }
+} else {
+	echo 'Fill out the form below to add your info.';
 }
 
-echo '<form action="/ahmed/customer_test.php" method="POST">
+?>
+<form action="/ahmed/customer_test.php" method="POST">
 Username: <input type="text" name="c_username"/><br/>
 Password: <input type="password" name="c_password"/><hr/>
 Full Name: <input type="text" name="c_name"/><br/>
 Email Address: <input type="text" name="c_email"/><br/>
 Phone Number: <input type="text" name="c_phone"/><br/>
-<input type="submit" name="f_submit" value="Create Account"/></form>';
-
-?>
+<input type="submit" name="f_submit" value="Create Account"/></form>
